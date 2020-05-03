4d60b36312394ed0391c1a5b1d5717a6ee3e04c5
opromio <oscar.pm7@gmail.com>
2020-05-03 15:50:34 +0200

    Corrected encoding issue

--- InformationTheory/CompressionAlgorithms/LZW/lzw_decompressor.py
+++ InformationTheory/CompressionAlgorithms/LZW/lzw_decompressor.py
@@ -10,15 +10,16 @@ import timeit
 ----------------------------  LZW DECOMPRESSOR  -------------------------------------
 -------------------------------------------------------------------------------------
 """
-with open("compressed_quijote.bin", "rb") as f, open("quijote_decompressed.txt", "w+") as output:
+
+with open("compressed_quijote.bin", "rb") as file, open("decompressed_quijote.txt", "w") as output:
     start = timeit.default_timer()
     bit_string = ""
-    byte = f.read(1)
+    byte = file.read(1)
     while len(byte) >= 1:  # Fem una string de la cadena de bits que hi ha al fitxer
         byte = ord(byte)
         bits = bin(byte)[2:].rjust(8, '0')
         bit_string += bits
-        byte = f.read(1)
+        byte = file.read(1)
 
     padded_info = bit_string[:8]
     extra_padding = int(padded_info, 2)
@@ -33,24 +34,21 @@ with open("compressed_quijote.bin", "rb") as f, open("quijote_decompressed.txt",
     text = chr(int(encoded[:8], 2))
     k = 0
     pbb = encoded[8:]
-    finished = False
-    while not finished:
+    while True:
         actual_k = 2 ** k
         for i in range(0, actual_k):
             pb = pbb[:k + 9]
             pbb = pbb[k + 9:]
-            # Quan ja no queda mes que llegir, escrivim el text.
-            if len(pb[:k + 1]) < 1 or len(pb[k + 1:]) < 1:
+
+            if len(pb[:k + 1]) < 1 or len(pb[k + 1:]) < 1:  # Quan ja no queda mes que llegir, escrivim el text.
                 output.write(text)
                 print(timeit.default_timer() - start)
-                finished = True
-                # aixo es el caracter que tenim al diccionari
-            word = str(dictionary[str(int(pb[:k + 1], 2))]) + chr(int(pb[k + 1:], 2))
+                exit()
+
+            word = str(dictionary[str(int(pb[:k + 1], 2))]) + chr(
+                int(pb[k + 1:], 2))  # aixo es el caracter que tenim al diccionari
             c += 1
             # guardem el codi binari associat a c, i el caracter que representen els 8
             dictionary.update({str(int(bin(c)[2:].zfill(k + 1), 2)): word})
             text += word
         k += 1
-
-    stop = timeit.default_timer()
-    print('Decompression time:', stop - start, '\n', 'Total time:', stop - start)
